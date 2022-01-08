f3a2f64e0aacd55f38b142f890e510a74d5fed17
yuskey38 <yskmyt.dev@gmail.com>
2022-01-09 04:09:00 +0900

    hook追加

--- lib/ui/article/views/article_list_view.dart
+++ lib/ui/article/views/article_list_view.dart
@@ -5,10 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // Project imports:
-import 'package:flutter_mvvm_template/l10n/l10n.dart';
 import 'package:flutter_mvvm_template/ui/article/states/article_list_view_state.dart';
 import 'package:flutter_mvvm_template/ui/article/view_models/article_list_view_model.dart';
 import 'package:flutter_mvvm_template/ui/article/views/article_list_tile.dart';
+import 'package:flutter_mvvm_template/ui/hook/use_l10n.dart';
 
 class ArticleListView extends HookConsumerWidget {
   const ArticleListView({Key? key}) : super(key: key);
@@ -17,9 +17,10 @@ class ArticleListView extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final viewModel = ref.watch(articleListViewModelProvider.notifier);
     final state = ref.watch(articleListViewModelProvider);
+    final l10n = useL10n();
 
     return Scaffold(
-        appBar: AppBar(title: Text(L10n.of(context)!.hello)),
+        appBar: AppBar(title: Text(l10n.hello)),
         body: RefreshIndicator(
             child: _buildListView(state),
             onRefresh: () => viewModel.fetchArticles()));
